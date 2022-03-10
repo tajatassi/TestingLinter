2903e32a20326e14d34935edfdd63c68fae1b20c
Taj Atassi <tajatassi@Tajs-MacBook-Pro.local>
2022-03-09 22:07:13 -0600

    a

---
+++ Something.py
@@ -0,0 +1,7 @@
+def someMethod:
+    return 7
+
+
+
+x = someMethod()
+print x
---
+++ file.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace some.linting.thing 
+{
+    class file {
+        static void main(string[] args) {
+            Console.WriteLine("hello")
+        }
+    }
+}
